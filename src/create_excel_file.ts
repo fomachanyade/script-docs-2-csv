d87196ea157c409ed036c8b74a0ad0bb318e534b
katakanakun <fomachanyade@gmail.com>
2025-02-23 21:37:17 +0900

    feat: add create_excel_file

---
+++ src/create_excel_file.ts
@@ -0,0 +1,41 @@
+import XLSX from 'xlsx';
+
+type CreateExcelFileArg = {
+    csvContent: string;
+    fileName: string;
+    xlsxFolderPath: string;
+}
+
+type CreateExcelFileReturn = {
+    xlsxPath: string;
+}
+
+const PROGRAM_NAME = "BOT_SCRIPT2CSV"
+
+export const createExcelFile = (arg: CreateExcelFileArg): CreateExcelFileReturn => {
+    const { csvContent, fileName, xlsxFolderPath } = arg;
+
+    const filePath = `${xlsxFolderPath}/${fileName}.xlsx`;
+    try{
+        console.log("start creating excel file. name:", fileName);
+        const workbook = XLSX.read(csvContent, { type: 'string' });
+        workbook.Props = {
+            Title: fileName,
+            Subject: PROGRAM_NAME,
+            Author: PROGRAM_NAME,
+            CreatedDate: new Date(),      // 作成日時
+            ModifiedDate: new Date(),     // 更新日時
+            // AppVersion は単なる文字列として扱われる場合が多い
+            // Excel 2013相当なら "15.0000", Excel 2016相当なら "16.0000" など
+            AppVersion: "15.0000",
+        };
+        XLSX.writeFile(workbook, filePath, { bookType: 'xlsx',  });
+
+        console.log(`Excel file created at ${filePath}`);
+        return { xlsxPath: filePath };
+    }catch(e){
+        console.error("Failed to create excel file. name:", fileName);
+        console.error(e);
+        throw e;
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
-const XLSX = require('xlsx');
-const { program } = require('commander');
-const fs = require('fs');
+import { program } from 'commander';
+import fs from 'fs';
+import XLSX from 'xlsx';
+
+import { createExcelFile } from './create_excel_file';
 
 program
-  .version('0.1.0')
-  .description('My CLI Tool')
+  .version('0.0.1')
+  .description('Create CSV and Excel file from TXT file')
   .action(() => {
     const csvContent = fs.readFileSync('./samples/output/csv/001_sample.csv', 'utf8');
     const workbook = XLSX.read(csvContent, { type: 'string' });
@@ -13,8 +15,6 @@ program
         Title: "TEST",
         Subject: "TEST SUBJECT",
         Author: "TEST AUTHOR",
-        Creator: "TEST CREATOR",         // SheetJSではAuthorとCreatorは別扱い
-        LastModifiedBy: "TEST Name",
         CreatedDate: new Date(),      // 作成日時
         ModifiedDate: new Date(),     // 更新日時
         // AppVersion は単なる文字列として扱われる場合が多い
@@ -22,6 +22,12 @@ program
         AppVersion: "15.0000",
     };
     XLSX.writeFile(workbook, './tmp/output_with_meta.xlsx', { bookType: 'xlsx',  });
+
+    const {xlsxPath} = createExcelFile({
+        csvContent: csvContent,
+        fileName: "001_sample",
+        xlsxFolderPath: './tmp'
+    })
   });
 
 program.parse(process.argv);
