6a18207bfcdf1deb6b040a8121b24b484ce050e1
katakanakun <fomachanyade@gmail.com>
2025-02-23 23:51:10 +0900

    feat: add parse_docs_text

---
+++ src/parse_docs_text.test.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it } from "vitest";
+import { buildChapterTitle, parseDocsText } from "./parse_docs_text";
+
+const simpleText = `
+                1
+                主人公だれだ
+                敵私だ
+                主人公お前だったのか
+            `
+const simpleTextPrefix = ["主人公", "敵"]
+
+const multipleChapterText = `
+            1
+            主人公だれだ
+            2
+            敵私だ
+            3
+            主人公お前だったのか
+        `
+const multipleChapterTextPrefix = ["主人公", "敵"]
+
+describe("parse_docs_test", () => {
+    describe("章が一つの場合", ()=>{
+        it("章わけの個数が正しくなされている", ()=>{
+            
+            const result = parseDocsText({
+                scriptStartPrefix: simpleTextPrefix,
+                docsText: simpleText
+            })
+
+            expect(result.chapters.length).equal(1)
+        })
+        it("章の配列が正しい", ()=>{
+            
+            const result = parseDocsText({
+                scriptStartPrefix: simpleTextPrefix,
+                docsText: simpleText
+            })
+            const resultChapterTiles = result.chapters.map((chapter) => chapter.title)
+            expect(resultChapterTiles).toEqual(["001_1"])
+        })
+
+        it("章の内容が正しい", ()=>{
+            const result = parseDocsText({
+                scriptStartPrefix: simpleTextPrefix,
+                docsText: simpleText
+            })
+            expect(result.chapters[0].content).toEqual([["主人公", "だれだ"], ["敵", "私だ"],["主人公", "お前だったのか"]])
+        })
+    });
+
+    describe("章が複数の場合", ()=>{
+        it("章わけの個数が正しくなされている", ()=>{
+            
+            const result = parseDocsText({
+                scriptStartPrefix: multipleChapterTextPrefix,
+                docsText: multipleChapterText
+            })
+
+            expect(result.chapters.length).equal(3)
+        })
+        it("章の配列が正しい", ()=>{
+            
+            const result = parseDocsText({
+                scriptStartPrefix: multipleChapterTextPrefix,
+                docsText: multipleChapterText
+            })
+            const resultChapterTiles = result.chapters.map((chapter) => chapter.title)
+            expect(resultChapterTiles).toEqual(["001_1", "002_2", "003_3"])
+        })
+
+        it("章の内容が正しい", ()=>{
+            const result = parseDocsText({
+                scriptStartPrefix: simpleTextPrefix,
+                docsText: multipleChapterText
+            })
+            expect(result.chapters[0].content).toEqual([["主人公", "だれだ"]] )
+            expect(result.chapters[1].content).toEqual([ ["敵", "私だ"]])
+            expect(result.chapters[2].content).toEqual([["主人公", "お前だったのか"]])
+        })
+    
+    });
+    
+})
+
+describe("buildChapterTitle", ()=>{
+    it("章のタイトルが正しい", ()=>{
+        const result = buildChapterTitle(0, "主人公")
+        expect(result).toBe("001_主人公")
+    })
+})
---
+++ src/parse_docs_text.ts
@@ -0,0 +1,55 @@
+export const buildChapterTitle = (num:number, line:string) =>{
+    const chapterPrefix = (num + 1).toString().padStart(3, "0");
+    return `${chapterPrefix}_${line}`;
+}
+
+
+type ParseDocsTextArgs = {
+    scriptStartPrefix: string[];
+    docsText: string;
+}
+
+type Chapter = {
+    title: string;
+    // 人名とセリフに分けられた行の配列
+    content: string[][];
+}
+
+type ParseDocsTextReturn = {
+    chapters: Chapter[];
+}
+
+export const parseDocsText = (
+    arg: ParseDocsTextArgs
+): ParseDocsTextReturn => {
+    const { scriptStartPrefix, docsText } = arg;
+
+    const chapters: Chapter[] = [];
+
+    const lines = docsText.split("\n").map(line=> line.trim());
+
+    let currentChapter: Chapter | null = null;
+
+    for(let i = 0; i < lines.length; i++){
+        const line = lines[i];
+
+        if(!line) continue;
+
+        const matchedPrefix = scriptStartPrefix.find((prefix) => line.startsWith(prefix));
+        if(!matchedPrefix){
+            
+            currentChapter = {
+                title: buildChapterTitle(chapters.length, line),
+                content: [],
+            };
+            chapters.push(currentChapter)
+        }else{
+            if(currentChapter){
+                const lineArray = line.split(matchedPrefix)
+                const lineContent = [matchedPrefix, lineArray[1]]; 
+                currentChapter.content.push(lineContent)
+            }
+        }    
+    }
+    return { chapters };
+}
